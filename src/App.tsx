bb7310f2497fc5d8b8e1affbe3720fb798c7ad6a
adhiru60 <adhirunath@gmail.com>
2023-05-22 23:34:44 +0530

    ""

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
  
  <BrowserRouter>
  <Header></Header>
- <h1>h</h1>
+ <h1>hi</h1>
  <Routes>
-  <Route path="application" element={<Application/>}/>
+  <Route path="/application" element={<Application/>}/>
  </Routes>
  </BrowserRouter>
  </>)
